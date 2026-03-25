8f93058beecbc7110c9a99ab060ba76d01a29535
watsonm19 <watsonm19@me.com>
2026-03-25 14:01:12 -0700

    Adjusted difficulty times + longer jump at end of strange world level

--- Assets/Scripts/GameDifficulty.cs
+++ Assets/Scripts/GameDifficulty.cs
@@ -8,8 +8,8 @@ public static class GameDifficulty
 {
     public const  int      RocketSprintIndex = 5;
     public static readonly string[] Names      = { "Stroll", "Jog", "Run", "Sprint", "Rocket Sprint", "Nova Sprint" };
-    public static readonly float[]  TimeLimits = { 1800f, 150f, 130f, 120f, 115f, 111f };
-    public static readonly string[] TimeLabels = { "30:00", "2:30", "2:10", "2:00", "1:55", "1:51" };
+    public static readonly float[]  TimeLimits = { 1800f, 150f, 130f, 121f, 116f, 111f };
+    public static readonly string[] TimeLabels = { "30:00", "2:30", "2:10", "2:01", "1:56", "1:51" };
 
     /// <summary>Index of the currently selected difficulty (1 = Jog).</summary>
     public static int  Current            = 1;
